64c83621e8dbf9e81cb6a0bbef0f0286876bbf63
Wyatt Zantua <tuo18351@temple.edu>
2025-03-24 13:37:38 -0400

    containerizes test node.js app

---
+++ nodejs/server.js
@@ -0,0 +1,9 @@
+// test node.js application
+
+const ronin     = require( 'ronin-server' )
+const mocks     = require( 'ronin-mocks' )
+ 
+const server = ronin.server()
+ 
+server.use( '/', mocks.server( server.Router(), false, true ) )
+server.start()
