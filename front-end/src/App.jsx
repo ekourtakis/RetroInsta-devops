017cd07cb8fd65b19422175e8f07962faadb1352
Sley Chery <156947295+sleyChery@users.noreply.github.com>
2025-03-27 14:16:22 -0400

    Create a header with navigation bar

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
