bfa340acf5728e8212739595f2413f0d55619403
tuh14497 <tuh14497@temple.edu>
2025-03-28 13:06:55 -0400

    added hardcoded post and feed components

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import Navbar from "./components/Navbar"
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
       <Navbar />
-    </>
-  )
+      <div className="cameraicon">
+        <img 
+          src={`/testimage/cameraicon.webp`} 
+          alt="Camera" 
+          className="camera-icon" 
+        />
+      </div>
+      <h1>RetroInstagram</h1>
+      <div className="Posts">
+        <Feed />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
