bf571035bdd48556e691b1a6db40e7d8ee4a73cb
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-04-02 11:42:28 -0400

    refactored PostComponent out of Feed

--- front-end/src/components/Feed/Feed.tsx
+++ front-end/src/components/Feed/Feed.tsx
@@ -1,57 +1,30 @@
 import React from "react";
-import "./Feed.css"; // Import the CSS file
-import { Post } from '../../models/Post.ts'
+import "./Feed.css";
+import { Post } from '../../models/Post';
+import PostComponent from '../PostComponent/PostComonent';
 
 const posts: Post[] = [
   {
     id: 1,
     username: "first_name",
-    avatar: "testimage/avatar.jpeg",
-    image: "testimage/mountain.jpeg",
+    avatar: "/testimage/avatar.jpeg",
+    image: "/testimage/mountain.jpeg",
     description: "description 1!",
   },
   {
     id: 2,
     username: "second284",
-    avatar: "testimage/man.jpeg",
-    image: "testimage/bridge.jpeg",
+    avatar: "/testimage/man.jpeg",
+    image: "/testimage/bridge.jpeg",
     description: "this sentence is a test",
   },
 ];
-
-interface PostProps {
-  username: string;
-  avatar: string;
-  image: string;
-  description: string;
-}
-
-const PostComponent: React.FC<PostProps> = ({ username, avatar, image, description }) => {
-  return (
-    <div className="post">
-      <div className="post-header">
-        <img className="avatar" src={avatar} alt={username} />
-        <span className="username">{username}</span>
-      </div>
-      <img className="post-image" src={image} alt="Post" />
-      <div className="post-content">
-        <div className="post-actions">
-          <span className="like">❤️</span>
-          <span className="comment">💬</span>
-        </div>
-        <p>
-          <strong>{username}</strong> {description}
-        </p>
-      </div>
-    </div>
-  );
-};
 
 const Feed: React.FC = () => {
   return (
     <div className="feed">
       {posts.map((post) => (
-        <PostComponent key={post.id} {...post} />
+        <PostComponent key={post.id} post={post} />
       ))}
     </div>
   );
