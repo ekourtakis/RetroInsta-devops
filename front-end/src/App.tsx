85b07e449b98fd8df299cf84319e5409af4da0cb
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-04-02 11:52:21 -0400

    array of posts now passed from App to PostFeed

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,31 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import Feed from "./components/PostFeed/PostFeed";
+import PostFeed from "./components/PostFeed/PostFeed";
+import{ Post } from "./models/Post"
+
+const posts: Post[] = [
+  {
+    id: 1,
+    username: "first_name",
+    avatar: "/testimage/avatar.jpeg",
+    image: "/testimage/mountain.jpeg",
+    description: "description 1!",
+  },
+  {
+    id: 2,
+    username: "second284",
+    avatar: "/testimage/man.jpeg",
+    image: "/testimage/bridge.jpeg",
+    description: "this sentence is a test",
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="Posts">
-        <Feed />
+        <PostFeed posts={posts} />
       </div>
     </div>
   );
--- front-end/src/components/PostFeed/PostFeed.tsx
+++ front-end/src/components/PostFeed/PostFeed.tsx
@@ -3,24 +3,11 @@ import "./PostFeed.css";
 import { Post } from '../../models/Post';
 import PostComponent from '../PostComponent/PostComonent';
 
-const posts: Post[] = [
-  {
-    id: 1,
-    username: "first_name",
-    avatar: "/testimage/avatar.jpeg",
-    image: "/testimage/mountain.jpeg",
-    description: "description 1!",
-  },
-  {
-    id: 2,
-    username: "second284",
-    avatar: "/testimage/man.jpeg",
-    image: "/testimage/bridge.jpeg",
-    description: "this sentence is a test",
-  },
-];
+interface PostFeedProps {
+  posts: Post[]
+}
 
-const Feed: React.FC = () => {
+const Feed: React.FC<PostFeedProps> = ({ posts }) => {
   return (
     <div className="feed">
       {posts.map((post) => (
