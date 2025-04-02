115af56ff7bba9fcf7231304ab33e8df777f9478
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-04-02 09:31:46 -0400

    migrated navbar to typescript

---
+++ front-end/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import './Navbar.css'; 
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        RetroInsta
+      </div>
+      <ul className="navbar-links">
+        <button>Sign up or Log in</button>
+      </ul>
+    </nav>
+  );
+};
