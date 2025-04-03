63aedbaf850f499c9222c05acc1b0340b52b5a00
ekourtakis <ekourtakis@gmail.com>
2025-04-03 12:40:19 -0400

    added google login button

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import PostFeed from "./components/PostFeed/PostFeed";
-import{ Post } from "./models/Post"
+import { Post } from "./models/Post"
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const posts: Post[] = [
   {
@@ -20,14 +21,21 @@ const posts: Post[] = [
   },
 ]
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+if (!googleClientId) {
+  console.error("Error. VITE_GOOGLE_CLIENT_ID env variable not set.")
+}
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="Posts">
-        <PostFeed posts={posts} />
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <div className="App">
+        <Navbar />
+        <div className="Posts">
+          <PostFeed posts={posts} />
+        </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
