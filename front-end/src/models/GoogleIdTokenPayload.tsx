2f2e8dc6cd82dec724aff37bb6c38099a86c85ff
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-04-06 17:16:05 -0400

    removed unneded comment

--- front-end/src/models/GoogleIdTokenPayload.tsx
+++ front-end/src/models/GoogleIdTokenPayload.tsx
@@ -1,6 +1,5 @@
 import { JwtPayload } from 'jwt-decode';
 
-// Define the same payload interface or import it
 export interface GoogleIdTokenPayload extends JwtPayload {
   email?: string;
   email_verified?: boolean;
