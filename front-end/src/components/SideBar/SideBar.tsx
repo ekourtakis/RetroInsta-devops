9d91475f19bc0dc9f4aaf3d063c08e37a55d455a
Sley Chery <156947295+sleyChery@users.noreply.github.com>
2025-04-09 11:25:58 -0400

    Added MUI Icons to menu items

--- front-end/src/components/SideBar/SideBar.tsx
+++ front-end/src/components/SideBar/SideBar.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './SideBar.css';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home'; 
+import TravelExploreIcon from '@mui/icons-material/TravelExplore'; 
+import PersonIcon from '@mui/icons-material/Person'; 
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const SideBar: React.FC = () => {
   return (
@@ -8,21 +12,25 @@ const SideBar: React.FC = () => {
       <ul className="sidebar-menu">
         <li className="sidebar-item">
           <Link to="/" className="sidebar-link">
+          <HomeIcon className="sidebar-icon" /> 
               Home
           </Link>
         </li>
         <li className="sidebar-item">
           <Link to="/explore" className="sidebar-link">
+          <TravelExploreIcon className="sidebar-icon" />
             Explore
           </Link>
         </li>
         <li className="sidebar-item">
           <Link to="/profile" className="sidebar-link">
+          <PersonIcon className="sidebar-icon" />
            Profile
           </Link>
         </li>
         <li className="sidebar-item">
           <Link to="/addPost" className="sidebar-link">
+          <AddBoxIcon className="sidebar-icon" />
             Add Post
           </Link>
         </li>
